269f921b0a81e74f0224e32e86d31f8d261e9225
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-07-11 04:02:53 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Z.K   on 11/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationsTests/AnimationsTests.swift
@@ -0,0 +1,19 @@
+//
+//  AnimationsTests.swift
+//  AnimationsTests
+//
+//  Created by Z.K   on 11/07/2026.
+//
+
+import Testing
+@testable import Animations
+
+struct AnimationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
