b6e2af72ad696277d6fe07c02b0d540476166599
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-07-14 15:59:22 -0700

    updated

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,27 +11,46 @@ struct ContentView: View {
     @State private var animationAmount: CGFloat = 1.0
     
     var body: some View{
-        Button("Tap Me"){
-            //animationAmount += 1.0
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(Color.white)
-        .clipShape(.circle)
-        //.scaleEffect(animationAmount)
-        //.animation(.linear, value: animationAmount)
-        .overlay(
-            Circle()
-                .stroke(Color.black)
+        VStack{
+            //            Button("Tap Me"){
+            //                //animationAmount += 1.0
+            //            }
+            //            .padding(50)
+            //            .background(.red)
+            //            .foregroundStyle(Color.white)
+            //            .clipShape(.circle)
+            //            //.scaleEffect(animationAmount)
+            //            //.animation(.linear, value: animationAmount)
+            //            .overlay(
+            //                Circle()
+            //                    .stroke(Color.black)
+            //                    .scaleEffect(animationAmount)
+            //                    .opacity(2-animationAmount)
+            //                    .animation(
+            //                        .easeInOut(duration: 2)
+            //                        .repeatForever(autoreverses: false),value: animationAmount
+            //                    )
+            //            )
+            //            .onAppear(){
+            //                animationAmount = 2
+            //            }
+            //
+            // control binding
+            
+        VStack {
+                Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+                
+                Spacer()
+                
+            Button("Tap Me") {
+                    animationAmount += 1
+                }
+                .padding(40)
+                .background(.red)
+                .foregroundStyle(.white)
+                .clipShape(.circle)
                 .scaleEffect(animationAmount)
-                .opacity(2-animationAmount)
-                .animation(
-                    .easeInOut(duration: 2)
-                    .repeatForever(autoreverses: false),value: animationAmount
-                )
-        )
-        .onAppear(){
-            animationAmount = 2
+            }
         }
     }
 }
