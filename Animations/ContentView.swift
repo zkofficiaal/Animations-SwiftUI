4ee08ca3eb2019a413214898ce98271d40e3f19b
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-07-14 16:09:06 -0700

    advanced bindign animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -36,9 +36,14 @@ struct ContentView: View {
             //            }
             //
             // control binding
+            print(animationAmount)
+            return
             
         VStack {
-                Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+                Stepper("Scale amount", value: $animationAmount.animation(
+                    .easeInOut(duration: 1)
+                    .repeatCount(5, autoreverses: true)
+                ), in: 1...10)
                 
                 Spacer()
                 
